2ed6f83b5691e12ab7c5b67af3914a1fb394b03f
thanveer <thanveer761@gmail.com>
2022-10-25 13:21:48 +0530

    more solved problems

--- day2/ClosestOccuranceOfCharacter.java
+++ day2/ClosestOccuranceOfCharacter.java
@@ -50,32 +50,66 @@ public class ClosestOccuranceOfCharacter {
 
 
 	
+	
+
+	
 	@Test
 	public void testCase1() {
-		String s="aaab", c="b";
+		String s="aaab";
+		char c='b';
 		Assert.assertArrayEquals(closestOccu(s,c),new int[] {3,2,1,0});
 	}
 	@Test
 	public void testCase2() {
-		String s="looveleetcode", c="e";
+		String s="loveleetcode";
+		char c='e';
 		Assert.assertArrayEquals(closestOccu(s,c),new int[] {3,2,1,0,1,0,0,1,2,2,1,0});
 	}
-
-
+	
 	/*pseudocode:
 	 * crreate a new array length for string s 
 	 * traverse through the loop
-	 * declare a substring from 0 to start index
-	 * declare another substring for s
-	 * declare a last of index of c string as left
-	 * get the whole index substring2
-	 * if left >-1 && right >-1
 	 * 
-	 * 
+	 *
+	 */
+
+	//2 pointers
+	public int[] closestOccu(String s,char c) {
+		int len = s.length();
+		int [] op = new int[len];
+		int index = -len;
+		for (int i = 0; i < len; i++) {
+			if(s.charAt(i)==c) {
+				index=i;
+			}
+			op[i]=i-index;
+		}
+		for (int  i= len-1;i>= 0; i--) {
+			if(s.charAt(i)==c) {
+				index=i;
+			}
+			
+			op[i]=Math.min(op[i], Math.abs(i-index));
+		}
+		
+		
+		
+		return op;
+		
+	}
+	
+	/*s="loveleetcode"
+	 * -12,-11,-10,0,1,0,0,1,2,3,4,0
+	 * 3,2,1,0,1,0,0,1,2,2,1,0
 	 * 
 	 */
+	
+	
+	
+	
+	
 	//bruteForce
-	public int[] closestOccu(String s,String c) {
+/*	public int[] closestOccu(String s,String c) {
 		int[] result = new int[s.length()];
 		for (int i = 0; i < result.length; i++) {
 			String substring = s.substring(0, i);
@@ -93,6 +127,6 @@ public class ClosestOccuranceOfCharacter {
 
 		return result;
 
-	}
+	}*/
 	
 }
--- day2/LongPressedName.java
+++ day2/LongPressedName.java
@@ -5,90 +5,100 @@ import org.junit.Test;
 
 public class LongPressedName {
 	/*
-	 * 1) Did I understand the problem? Yes 
-	 * 		-> If yes, go to next step !!
+	 * 1) Did I understand the problem? Yes -> If yes, go to next step !!
 	 * 
-	 *    What is the input(s)?
-	 *    What is the expected output?
-	 *    Do I have constraints to solve the problem?
-	 *    Do I have all informations to go to next step!!
-	 *    How big is your test data set will be?
-	 *    
+	 * What is the input(s)? What is the expected output? Do I have constraints to
+	 * solve the problem? Do I have all informations to go to next step!! How big is
+	 * your test data set will be?
+	 * 
 	 * 2) Test data set
-	 *  	
-	 *  	Minimum of 3 data set !! Positive, Edge, Negative 
-	 *      Validate with the interviewer if the data set is fine by his/her assumptions
+	 * 
+	 * Minimum of 3 data set !! Positive, Edge, Negative Validate with the
+	 * interviewer if the data set is fine by his/her assumptions
 	 * 
 	 * 3) Do I know how to solve it?
-	 * 		
-	 * 		Yes - great, is there an alternate?
+	 * 
+	 * Yes - great, is there an alternate?
 	 * 
 	 * 4) Ask for hint (If you do not know how to solve)
-	 *  
+	 * 
 	 * 5) Do I know alternate solutions as well? No
 	 * 
-	 * 		No - That is still fine, proceed to solve by what you know !!
+	 * No - That is still fine, proceed to solve by what you know !!
 	 * 
 	 * 6) If you know alternate solutions -> find out the O Notations (Performance)
 	 * 
-	 * 		Then, explain either both or the best (depends on the time)
+	 * Then, explain either both or the best (depends on the time)
 	 * 
-	 * 		Approach 1: Start with worst -> Improve (Optimize) -> End up with the best
-	 * 		Approach 2: Write down the options and benefits and code the best 
+	 * Approach 1: Start with worst -> Improve (Optimize) -> End up with the best
+	 * Approach 2: Write down the options and benefits and code the best
 	 * 
-	 * 7) Start always with Psuedo code 
+	 * 7) Start always with Psuedo code
 	 * 
 	 * 8) Implement them in the code (editor)
 	 * 
-	 * 9) Test against the different data set 
+	 * 9) Test against the different data set
 	 * 
 	 * 10) If it fails, debug them to solve it !!
 	 * 
 	 */
 
-
 	@Test
 	public void testCase0() {
-		String name="alex", typed="aaleex";
-		Assert.assertEquals(longPressedName(name,typed), true);
+		String name = "alex", typed = "aaleex";
+		Assert.assertEquals(longPressedName(name, typed), true);
 	}
+
 	@Test
 	public void testCase1() {
-		String name="khan", typed="khaan";
-		Assert.assertEquals(longPressedName(name,typed), true);
+		String name = "khan", typed = "khaan";
+		Assert.assertEquals(longPressedName(name, typed), true);
 	}
 
 	@Test
 	public void testCase2() {
-		String name="saeed", typed="ssaaedd";
-		Assert.assertEquals(longPressedName(name,typed), false);
+		String name = "saeed", typed = "ssaaedd";
+		Assert.assertEquals(longPressedName(name, typed), false);
 	}
 
 	@Test
 	public void testCase3() {
-		String name="thanveer", typed="ahmed";
-		Assert.assertEquals(longPressedName(name,typed), false);
+		String name = "thanveer", typed = "ahmed";
+		Assert.assertEquals(longPressedName(name, typed), false);
 	}
 
-	/*pseudocode:
-	 *traverse through name and typed string
-	 *if name and typed contains more than on character if name string
-	 *return true 
-	 *else return false
-	 * 
+	/*
+	 * pseudocode: 
+	 * initialize i & j variables as 0
+	 * if name.length equal to  type length return true
+	 * if name.length greater than  type length return true
+	 *  if name.charArt[i] matches with  typed.charAt[j] increment i&j
+	 * if  typed.charAt[0] is not eqauls to  typed.charAt[1st] index return false;
+	 * if i == name.lenght return true 
 	 */
-	//bruteForce--> TC:-O[N];	sc:-O[1]
-	public boolean longPressedName(String name,String typed) {
-		int origName = name.length();
-		int typedName = typed.length();
-		for (int i = 0; i < origName; i++) {
-			for (int j = 0; j < typedName; j++) {
-				if(name.charAt(i)!=typed.charAt(i)) {
-					return true;
-				}else 
-					return false;
-			}
-			}
+	// bruteForce--> TC:-O[N]; sc:-O[1]
+	public boolean longPressedName(String name, String typed) {
+
+		int i = 0, j = 0;
+
+		if (name.length() == typed.length())
+			return name.equals(typed);
+
+		if (name.length() > typed.length())
 			return false;
-		
-		}}
+
+		while (j < typed.length()) {
+			if (i < name.length() && name.charAt(i) == typed.charAt(j)) {
+
+				i++;
+
+			} else if (j == 0 || typed.charAt(j) != typed.charAt(j - 1)) {
+				return false;
+			}
+			j++;
+		}
+
+		return i == name.length();
+
+	}
+}
