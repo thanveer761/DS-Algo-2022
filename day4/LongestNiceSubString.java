2840d72f53c7b2dbcea9b1913d671dc0de7b1c2c
thanveer <thanveer761@gmail.com>
2022-11-07 10:50:30 +0530

    more solved problems

--- day4/LongestNiceSubString.java
+++ day4/LongestNiceSubString.java
@@ -1,5 +1,8 @@
 package day4;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.Test;
 
 public class LongestNiceSubString {
@@ -9,7 +12,8 @@ public class LongestNiceSubString {
 	public void testCase1() {
 		String s="YazaAay";
 		
-		longestNiceSubString(s);
+		String op = longestNiceSubString(s);
+		System.out.println(op);
 	}
 	
 	/*Given a string s and a number k, find the maximum number of vowels in substring of size k.
@@ -22,19 +26,23 @@ public class LongestNiceSubString {
 //2 pointers
 	 public String longestNiceSubString(String s) {
 		 
-	String op="";
-		 for (int i = 0; i < s.length(); i++) {
-			for (int j = i+1; j < s.length(); j++) {
-				if (s.charAt(i)!=s.charAt(j)) {
-					i++;
-					j++;
-				}else
-					
-			}
-		}
-		 
-		 System.out.println(op);
-		return op;
+		 if(s.length()<2) return "";  
+	        
+	        Set<Character> set = new HashSet<>();
+	        for(char c : s.toCharArray())  set.add(c);
+	        
+	        for(int i=0;i<s.length();i++){
+	            if(set.contains(Character.toUpperCase(s.charAt(i))) &&  set.contains(Character.toLowerCase(s.charAt(i)))){
+	                continue;
+	            }
+	            
+	            String s1 = longestNiceSubString(s.substring(0,i));
+	            String s2 = longestNiceSubString(s.substring(i+1));
+	            
+	            return s1.length()>=s2.length() ? s1 : s2;
+	        }
+	        
+	        return s;
 		
 	 }
 }
