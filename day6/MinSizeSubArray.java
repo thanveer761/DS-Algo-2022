a4ee8db0567bf1e540ce8e0f45245de50b02fd69
thanveer <thanveer761@gmail.com>
2022-11-08 19:16:31 +0530

    more solved problems

---
+++ day6/MinSizeSubArray.java
@@ -0,0 +1,34 @@
+package day6;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MinSizeSubArray {
+	@Test
+	public void testCase1() {
+		int[] input = { 2, 3, 1, 2, 4, 3 };
+		int tar = 7;
+		int prac = prac(input, tar);
+		System.out.println(prac);
+	}
+
+	public int prac(int[] nums, int tar) {
+
+		int left = 0, right = 0, sum = 0;
+		int min = Integer.MAX_VALUE;
+		while (right < nums.length) {
+			sum += nums[right];
+			if (sum >= tar) {
+				while (sum >= tar) {
+					sum -= nums[left++];
+					
+				}
+				min = Math.min(tar, right - left + 2);
+			}
+			right++;
+		}
+		return min;
+
+	}
+
+}
---
+++ day6/MostCommonWords.java
@@ -0,0 +1,60 @@
+package day6;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class MostCommonWords {
+
+	@Test
+	public void testCase1() {
+		String paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+		String[] banned = { "hit" };
+		String op = mostCommWords(paragraph, banned);
+		System.out.println(op);
+	}
+	/*public String mostCommWords(String paragraph, String[] banned) {
+
+		Set<String> Banned_Words = new HashSet<String>();
+		Map<String, Integer> Curr_Words = new HashMap<String, Integer>();
+		for (String BanWord : banned) {
+			Banned_Words.add(BanWord);
+		}
+
+		String[] words = paragraph.toLowerCase().split("\\W+");
+		for (String currWord : words) {
+			if (!Banned_Words.contains(currWord)) {
+				Curr_Words.put(currWord, Curr_Words.getOrDefault(currWord, 0) + 1);
+			}
+		}
+		int max = 0;
+		String result = "";
+		Set<String> keySet = Curr_Words.keySet();
+		for (String currWord : keySet) {
+			if (Curr_Words.get(currWord) > max) {
+				max = Curr_Words.get(currWord);
+				result = currWord;
+			}
+
+		}
+		return result;
+	}*/
+	
+	public String mostCommWords(String paragraph, String[] banned) {
+	
+	String[] words = paragraph.toLowerCase().split("\\W+");
+    HashMap<String, Integer> map = new HashMap<>();
+    for(String word : words) map.put(word, map.getOrDefault(word, 0) + 1);
+    for(String word : banned) if(map.containsKey(word)) map.remove(word);
+    String res = null;
+    for(String word : map.keySet())
+        if(res == null || map.get(word) > map.get(res))
+            res = word;
+    return res;
+}
+}
