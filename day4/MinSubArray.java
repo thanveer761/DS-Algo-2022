f287de2b132cee0f1539a3aa3fe87a1b775e22f5
thanveer <thanveer761@gmail.com>
2022-11-17 18:28:43 +0530

    solved problem

---
+++ day4/MinSubArray.java
@@ -0,0 +1,22 @@
+package day4;
+
+public class MinSubArray {
+	 
+	 
+	 public void td1(){
+		int[] nums = {2,3,1,2,4,3};
+		int target = 7;
+	 }
+	 
+	 public int[] minSubArr(int[] nums, int target){
+		 
+		 
+		 
+		 
+		return nums;
+	 
+	
+
+	 
+	 }
+}
